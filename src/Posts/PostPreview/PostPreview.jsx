bca6eba5855a563afb4c558560a3e6f409c6eacf
lordviet <viet1231@abv.bg>
2020-05-14 02:26:43 +0300

    fixed unescaped titles

--- src/Posts/PostPreview/PostPreview.jsx
+++ src/Posts/PostPreview/PostPreview.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import './PostPreview.css';
+import StringExtended from 'string';
 
 function PostPreview(props) {
-    console.log(props);
+    
     const { title, excerpt, thumbnail } = props;
-    console.log(typeof excerpt)
+    
     return (
         <div className="post-preview">
             <img src={thumbnail} alt="thumbnail" />
             <div className="post-preview-info">
-                <h3>{title}</h3>
+                <h3>{StringExtended(title).unescapeHTML().s}</h3>
                 <p dangerouslySetInnerHTML={{ __html: excerpt }}></p>
                 <p>Read More</p>
             </div>
