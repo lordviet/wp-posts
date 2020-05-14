ff55ecaf65a35accf62997f7264384f0877eecf1
lordviet <viet1231@abv.bg>
2020-05-15 02:41:27 +0300

    better convention

--- src/Posts/Posts.jsx
+++ src/Posts/Posts.jsx
@@ -22,13 +22,13 @@ export default class Posts extends Component {
 
     componentDidUpdate(prevProps, prevState) {
         // moreLoaded in state to show a loader?
-        if(prevState.nextPage !== this.state.nextPage){
+        if (prevState.nextPage !== this.state.nextPage) {
             axios.get(`/wp-json/wp/v2/posts?post_type=post&count=10&page=${this.state.nextPage}`)
-            .then(res => this.setState({
-                posts: [...this.state.posts, ...res.data],
-                hasLoaded: true,
-            }))
-            .catch(e => console.log(e));
+                .then(res => this.setState({
+                    posts: [...this.state.posts, ...res.data],
+                    hasLoaded: true,
+                }))
+                .catch(e => console.log(e));
         }
     }
 
@@ -40,8 +40,15 @@ export default class Posts extends Component {
         return (
             <div className="posts">
                 {hasLoaded ?
-                    posts.map(p => <PostPreview key={p.id} postId={p.id} title={p.title.rendered}
-                        excerpt={p.excerpt.rendered} thumbnail={p.jetpack_featured_media_url} />) :
+                    posts.map(p => (
+                        <PostPreview
+                            key={p.id}
+                            postId={p.id}
+                            title={p.title.rendered}
+                            excerpt={p.excerpt.rendered}
+                            thumbnail={p.jetpack_featured_media_url}
+                        />
+                    )) :
                     <div>Loading...</div>}
                 <button onClick={this.loadMoreOnClick}> Load More </button>
             </div>
