9146220d1b3a56b6d1258ce5e9be4d08456ab11e
lordviet <viet1231@abv.bg>
2020-05-15 18:20:44 +0300

    added styles to post preview

--- src/Posts/PostPreview/PostPreview.jsx
+++ src/Posts/PostPreview/PostPreview.jsx
@@ -3,17 +3,21 @@ import './PostPreview.css';
 import StringExtended from 'string';
 import { Link } from 'react-router-dom';
 
-function PostPreview({ title, excerpt, thumbnail, postId }) {
+export default function PostPreview({ title, excerpt, thumbnail, postId }) {
     return (
-        <div className="post-preview">
+        <Link to={`/post/${postId}`} className="post-preview">
             <div className="post-preview-info">
                 <h3>{StringExtended(title).unescapeHTML().s}</h3>
                 <p dangerouslySetInnerHTML={{ __html: excerpt }}></p>
-                <Link to={`/post/${postId}`}>Read More</Link>
             </div>
-            <img src={thumbnail} alt="thumbnail" />
-        </div>
+            <picture>
+                <source media="(min-width: 327px)"
+                    type="image/jpeg" srcSet={thumbnail} />
+                <source type="image/jpeg"
+                    srcSet={thumbnail} />
+                <img src={thumbnail} alt="thumbnail"
+                    width="280" height="260" />
+            </picture>
+        </Link>
     );
 }
-
-export default PostPreview;
