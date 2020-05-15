767328640820eb7e9c9f8c37c420288aed02abeb
lordviet <viet1231@abv.bg>
2020-05-15 19:06:21 +0300

    added not found page

--- src/App/App.js
+++ src/App/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Posts from '../Posts/Posts';
 import PostScroll from '../PostScroll/PostScroll';
 import Header from '../Header/Header';
+import NotFound from '../NotFound/NotFound';
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,10 +14,11 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Header/>
+        <Header />
         <Switch>
           <Route exact path='/' component={Posts} />
           <Route path='/post/:id' component={PostScroll} />
+          <Route path='/*' component={NotFound} />
         </Switch>
       </div>
     </Router>
